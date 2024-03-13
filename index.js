c0428668839f8a80708e0bbb0a47065a2d90a8ac
Arinze Anakor <arinzeanakor@gmail.com>
2024-03-13 17:21:22 +0100

    refactor email sender

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
+var cors = require('cors')
 const { sendEmail } = require('./mail');
 const app = express();
 
 const helmet = require('helmet')
 app.use(helmet({
 	crossOriginEmbedderPolicy: process.env.NODE_ENV !== 'development'
-}))
+}));
+
+const whitelist = [process.env.FRONTEND_APP_URL];
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (process.env.NODE_ENV === 'development') {
+			return callback(null, true)
+		}
+
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	}
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -14,11 +32,17 @@ app.get('/', (_, res) => {
 });
 
 app.post('/send-email', (req, res) => {
+	const { name, email } = req.body;
+
+	const receipients = ` ${name} <${email}>`;
+	const subject = `Welcome to our website`;
+	const message = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus mollitia nam, quo exercitationem cumque dolor molestias quis ipsam voluptatum sunt obcaecati explicabo eveniet quaerat corrupti similique optio, ratione ad at!`
+
 	res.json({ message: 'Sending email in a moment...' });
 
-	const { receipients, subject, message } = req.body;
-
-	sendEmail({ receipients, subject, message });
+	sendEmail({ receipients, subject, message })
+		.then(result => {})
+		.catch(error => console.log(`Unable to send email to ${JSON.stringify({ receipients })}`));
 });
 
 module.exports = app;
--- mail.js
+++ mail.js
@@ -11,18 +11,13 @@ const mailTransport = nodemailer.createTransport({
 });
 
 const sendEmail = async ({ receipients, subject, message }) => {
-	try {
-		const info = await mailTransport.sendMail({
-			from: process.env.MAIL_SENDER_DEFAULT, // sender address
-			to: receipients, // list of receivers
-			subject: subject, // Subject line
-			text: message, // plain text body
-		});
-
-		return info;
-	} catch (error) {
-		console.log(`Unable to send email to ${JSON.stringify({receipients})}`)
-	}
+	return await mailTransport.sendMail({
+		from: process.env.MAIL_SENDER_DEFAULT, // sender address
+		to: receipients, // list of receivers
+		subject: subject, // Subject line
+		text: message, // plain text body
+		html: message, // valid text body
+	});
 }
 
-module.exports = { sendEmail};
+module.exports = { sendEmail };
